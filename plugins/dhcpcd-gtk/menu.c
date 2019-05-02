af0ba62059f032a1a1840366ff3a17d7e2b75fb2
Simon Long <simon@raspberrypi.org>
2019-05-02 10:42:06 +0100

    Use raspi-config to check whether wifi country is set

--- plugins/dhcpcd-gtk/menu.c
+++ plugins/dhcpcd-gtk/menu.c
@@ -517,7 +517,7 @@ static int wifi_country_set (void)
     if (pclose (fp)) return 1;
 
     // is the country set?
-    fp = popen ("iw reg get | grep -q 'country [A-Z][A-Z]:'", "r");
+    fp = popen ("raspi-config nonint get_wifi_country", "r");
     if (pclose (fp)) return 0;
 
     return 1;
