e5d9c8fc100531ab10518e05a59c929c92035a0f
Simon Long <simon@raspberrypi.com>
2020-04-28 12:07:25 +0100

    Tidied

--- plugins/dhcpcd-gtk/main.c
+++ plugins/dhcpcd-gtk/main.c
@@ -779,9 +779,9 @@ static GtkWidget *dhcpcdui_constructor (LXPanel *panel, config_setting_t *settin
 
 #ifdef ENABLE_NLS
     setlocale (LC_ALL, "");
-    bindtextdomain ( GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR );
-    bind_textdomain_codeset ( GETTEXT_PACKAGE, "UTF-8" );
-    textdomain ( GETTEXT_PACKAGE );
+    bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+    bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+    textdomain (GETTEXT_PACKAGE);
 #endif
 
     dhcp->tray_icon = gtk_image_new ();
